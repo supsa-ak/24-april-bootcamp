2f3ad9cb4e65923333bba18131d9e018402bd8ec
Sarthak <sarthakkalpande113@gmail.com>
2022-04-24 12:28:27 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Hello Harshit');
+
+function sendMsg(){
+    document.getElementById('message').innerHTML = "Button Clicked";
+    document.getElementById('message').style.display = "block";
+}
